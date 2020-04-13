dc275ca19a69eda1b940ecc5993ad58459eefe2a
Jesse B. Hannah <jesse@jbhannah.net>
2020-04-13 11:27:58 -0700

    Fix macOS key repeat in VS Code

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,8 @@ if echo $platform | grep -q Darwin; then
   brew bundle
   [[ -f /etc/zshenv ]] && sudo mv /etc/zshenv /etc/zshrc
   [[ -f /etc/zprofile ]] && sudo mv /etc/zprofile "/etc/zprofile~orig"
+
+  defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 elif echo $platform | grep -q Linux; then
   echo "== Ubuntu Installation =="
   sudo apt-get install build-essential zsh vim-nox luajit python3 python3-dev python3-pip silversearcher-ag stow exuberant-ctags stow

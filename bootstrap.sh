a17783ceddbc5f0d715ce0415627f88489748a63
Jesse B. Hannah <jesse@jbhannah.net>
2020-11-07 08:58:58 -0700

    Update bootstrap script for running via curl

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,26 +4,28 @@ platform=`uname -a`
 
 echo "= Dotfiles Setup ="
 
-echo "== Initialize Submodules =="
-git submodule update --recursive --init
-
 if echo $platform | grep -q Darwin; then
-  echo "== macOS Installation =="
-
   if ! command -v brew >/dev/null 2>&1; then
     echo "=== Installing Homebrew ==="
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   fi
 
-  brew bundle --file common/.Brewfile
-  [[ -f /etc/zshenv ]] && sudo mv /etc/zshenv /etc/zshrc
-  [[ -f /etc/zprofile ]] && sudo mv /etc/zprofile "/etc/zprofile~orig"
-
   defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 elif echo $platform | grep -q Linux; then
-  echo "== Ubuntu Installation =="
+  echo "== Installing Dependencies =="
   sudo apt-get install build-essential zsh vim-nox luajit python3 python3-dev python3-pip silversearcher-ag stow exuberant-ctags stow
   sudo -H pip3 install thefuck
+fi
+
+echo "== Cloning Dotfiles Repository =="
+STOW_DIR=$HOME/Code/src/github.com/jbhannah/dotfiles
+git clone https://github.com/jbhannah/dotfiles.git $STOW_DIR
+cd $STOW_DIR
+git submodule update --recursive --init
+
+if echo $platform | grep -q Darwin; then
+  echo "== Installing Homebrew Packages =="
+  brew bundle --file common/.Brewfile
 fi
 
 echo "== Installing Dotfiles =="

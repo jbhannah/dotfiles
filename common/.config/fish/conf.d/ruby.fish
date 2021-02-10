9bc5973104c045624cc5bad9c55ac5704931fe2d
Jesse B. Hannah <jesse@jbhannah.net>
2021-02-10 10:25:08 -0800

    More Bundler defaults and abbreviations

--- common/.config/fish/conf.d/ruby.fish
+++ common/.config/fish/conf.d/ruby.fish
@@ -1,5 +1,7 @@
 if status is-interactive; and type -q bundle
     abbr -a -g rbb bundle
+    abbr -a -g rbba bundle add
     abbr -a -g rbbe bundle exec
     abbr -a -g rbbi bundle install
+    abbr -a -g rbbu bundle update
 end

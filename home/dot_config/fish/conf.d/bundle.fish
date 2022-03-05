6cbfb1bd7dc09c362753abf15b4e056f36ce030d
Jesse B. Hannah <jesse@jbhannah.net>
2022-03-05 15:34:52 -0800

    Add bundler and Go shell config

---
+++ home/dot_config/fish/conf.d/bundle.fish
@@ -0,0 +1,7 @@
+if status is-interactive; and command -sq bundle
+    abbr -a -g rbb bundle
+    abbr -a -g rbbA bundle add
+    abbr -a -g rbbe bundle exec
+    abbr -a -g rbbI bundle install
+    abbr -a -g rbbU bundle update
+end

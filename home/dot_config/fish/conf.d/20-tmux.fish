5d3d15cc925d43fcca8fe3e7df71c76cfd8b47f9
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-08-15 09:36:33 -0700

    Don't run tmux in Zed shell

--- home/dot_config/fish/conf.d/20-tmux.fish
+++ home/dot_config/fish/conf.d/20-tmux.fish
@@ -1,5 +1,6 @@
 status is-interactive
 and not set -q VSCODE_CWD
 and not string match -q "$TERM_PROGRAM" vscode
+and not string match -q "$TERM_PROGRAM" zed
 and not set -q TMUX
 and exec tmux new-session -As0
--- home/dot_config/fish/conf.d/jenv.fish
+++ home/dot_config/fish/conf.d/jenv.fish
@@ -1,6 +1,5 @@
 if status is-interactive
     if command -sq jenv
-        set PATH $HOME/.jenv/bin $PATH
-        jenv init - | source
+        jenv init - fish | source
     end
 end

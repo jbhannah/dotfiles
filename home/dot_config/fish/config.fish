61a5e10728dc87d770af946f32795f3bbe742795
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-12-23 00:16:17 -0800

    Load 1Password CLI plugins if configured

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     command -sq starship; and starship init fish | source
+
+    if test -f $HOME/.config/op/plugins.sh
+        source $HOME/.config/op/plugins.sh
+    end
 end

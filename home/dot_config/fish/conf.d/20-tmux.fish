88e07f54b7c0aa87222c3b71f97a9bdcad196252
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-12-22 21:25:33 -0800

    Disable tmux in Warp

--- home/dot_config/fish/conf.d/20-tmux.fish
+++ home/dot_config/fish/conf.d/20-tmux.fish
@@ -2,5 +2,6 @@ status is-interactive
 and not set -q VSCODE_CWD
 and not string match -q "$TERM_PROGRAM" vscode
 and not string match -q "$TERM_PROGRAM" zed
+and not string match -q "$TERM_PROGRAM" WarpTerminal
 and not set -q TMUX
 and exec tmux new-session -As0
